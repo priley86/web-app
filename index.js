27752a7eb8dd215c4aaebac9f422b321c29456b2
Patrick Riley <priley86@gmail.com>
2021-05-24 23:07:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,10 @@ app.use(
    authRequired: false,
    auth0Logout: true,
    baseURL: APP_URL,
+   authorizationParams: {
+     response_type: "code id_token",
+     audience: "https://expenses-api",
+   },
  })
 );
 
@@ -72,17 +76,25 @@ app.get("/user", requiresAuth(), async (req, res) => {
   });
 });
 
- app.get("/expenses", requiresAuth(), async (req, res, next) => {
-   try {
-     const expenses = await axios.get(`${API_URL}/reports`);
-     res.render("expenses", {
-       user: req.oidc && req.oidc.user,
-       expenses: expenses.data,
-     });
-   } catch (err) {
-     next(err);
-   }
- });
+app.get("/expenses", requiresAuth(), async (req, res, next) => {
+ try {
+   // 👇 get the token from the request 👇
+   const { token_type, access_token } = req.oidc.accessToken;
+   // 👇 then send it as an authorization header 👇
+   const expenses = await axios.get(`${API_URL}/reports`, {
+     headers: {
+       Authorization: `${token_type} ${access_token}`,
+     },
+   });
+   // 👆 end of changes 👆
+   res.render("expenses", {
+     user: req.oidc && req.oidc.user,
+     expenses: expenses.data,
+   });
+ } catch (err) {
+   next(err);
+ }
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
